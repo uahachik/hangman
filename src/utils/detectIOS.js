4d542dc651e98c7f60ea915f86eb154350638d3b
kotenkoroman2019 <kotenkoroman2019@gmail.com>
2020-09-03 09:18:33 +0300

    try to detect if device is IOS

--- src/utils/detectIOS.js
+++ src/utils/detectIOS.js
@@ -1,12 +1,25 @@
 export default () => {
-    return [
-      'iPad Simulator',
-      'iPhone Simulator',
-      'iPod Simulator',
-      'iPad',
-      'iPhone',
-      'iPod'
-    ].includes(navigator.platform)
-    // iPad on iOS 13 detection
-    || (navigator.userAgent.includes("Mac") && "ontouchend" in document);
+    // return [
+    //   'iPad Simulator',
+    //   'iPhone Simulator',
+    //   'iPod Simulator',
+    //   'iPad',
+    //   'iPhone',
+    //   'iPod'
+    // ].includes(navigator.platform)
+    // // iPad on iOS 13 detection
+    // || (navigator.userAgent.includes("Mac") && "ontouchend" in document);
+
+  const iosQuirkPresent = () => {
+    const audio = new Audio();
+
+    audio.volume = 0.5;
+    return audio.volume === 1;   // volume cannot be changed from "1" on iOS 12 and below
+  };
+
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const isAppleDevice = navigator.userAgent.includes('Macintosh');
+  const isTouchScreen = navigator.maxTouchPoints >= 1;   // true for iOS 13 (and hopefully beyond)
+
+  return isIOS || (isAppleDevice && (isTouchScreen || iosQuirkPresent()));
 }
